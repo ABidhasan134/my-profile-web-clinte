01a2be4611d9121ec75731eb2d66e6180c3850f0
ABid hasan <abeydhasan134@gmail.com>
2025-04-08 12:29:56 +0600

    cursor issu fix

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,13 @@ import router from './router/router.jsx';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import AuthProvider from './context/authProvider.jsx';
 import { HelmetProvider } from 'react-helmet-async';
-
+import AnimatedCursor from "react-animated-cursor"
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <HelmetProvider>
+    <AnimatedCursor color='30, 64, 175'/>
   <AuthProvider>
   <QueryClientProvider client={queryClient}>
   <RouterProvider router={router}>
